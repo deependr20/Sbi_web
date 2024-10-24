2afebcca27937bbb9e84d488e7a39aa8f93b88b8
deependr20 <138904154+deependr20@users.noreply.github.com>
2024-10-24 20:55:13 +0530

    Update BlogCard.jsx

--- src/Components/BlogCard.jsx
+++ src/Components/BlogCard.jsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 const BlogCard = ({ img, category, title, description, link }) => {
     return (
         <div className=" bg-white h-full border border-gray-200 rounded-lg shadow flex flex-col justify-between">
-            <img className="object-cover h-full" src={img} alt="" />
+             <div className='lg:h-[16rem]'>
+                  <img className="object-cover rounded-t-lg  w-full overflow-hidden h-full" src={img} alt="" />
+            </div>
             <div className="px-4  py-4">
                 <div className='flex gap-4  items-center mb-4'>
                     <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl  font-medium rounded-lg text-sm px-5 py-[.5rem] text-center ">
@@ -16,8 +18,7 @@ const BlogCard = ({ img, category, title, description, link }) => {
                 <p className=" font-medium text-sm text-gray-800">{description}</p>
             </div>
             <div className="flex items-center text-blue-700 text pb-4 gap-4">
-                <Link  className= "font-medium rounded-lg text-sm"></Link>
-                    Read more
+               <Link   className= "pl-4 hover:underline  font-medium rounded-lg text-sm">Read more</Link>
                 <div className='bg-white w-6 h-6 flex items-center justify-center rounded-full border border-blue-300'>
                     <i class="ri-arrow-right-line"></i>
                 </div>
